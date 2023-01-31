618322fac83785e74fde37d165bc48d38c78b16c
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2023-01-31 14:07:59 -0500

    create routers for all get, post, put, and delete tweets

---
+++ server/routes/tweetsRouters.js
@@ -0,0 +1,45 @@
+const router = require('express').Router();
+
+const { createTweet, getAllTweets, updateTweet, deleteTweet } = require('../queries/tweets');
+
+router.get('/', async (req, res) => {
+  const { success, data, message } = await getAllTweets();
+  if (success) {
+    res.send(data);
+  } else {
+    res.status(400).send({ message });
+  }
+});
+
+router.post('/', async (req, res) => {
+  const { content } = req.body;
+  const { success, data, message } = await createTweet(content);
+  if (success) {
+    res.send(data[0]);
+  } else {
+    res.status(400).send({ message });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { content } = req.body;
+  const { success, data, message } = await updateTweet(id, content);
+  if (success) {
+    res.send(data[0]);
+  } else {
+    res.status(400).send({ message });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { success, message } = await deleteTweet(id);
+  if (success) {
+    res.sendStatus(204);
+  } else {
+    res.status(400).send({ message });
+  }
+});
+
+module.exports = router;
